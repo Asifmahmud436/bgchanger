1e367143adc6bb36542501e0095b007b4c2ee53c
Zettex <safaandsafa4@gmail.com>
2025-01-19 14:45:34 +0600

    added two functions

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 function generateColor(){
-    alert("You generated a color")
+    let colorStr = '#'
+    for(let i = 1; i<7 ; i++){
+        colorStr+= Math.ceil(Math.random()*9)
+    }
+    return colorStr;
+}
+
+let container = document.querySelector('.boxes');
+function applyColor(){
+    for(let i = 0; i < container.children.length; i++){
+        container.children[i].style.backgroundColor = generateColor()
+        container.children[i].innerText = generateColor()
+    }
 }
