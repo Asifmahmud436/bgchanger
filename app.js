c2e6fecc172d1197ab1719ba0c238af4806b6094
Zettex <safaandsafa4@gmail.com>
2025-01-19 15:02:37 +0600

    now the background color also changes

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
+let container = document.querySelector('.boxes');
+let body = document.querySelector('body')
+
 function generateColor(){
     let colorStr = '#'
-    for(let i = 1; i<7 ; i++){
+    colorStr += Math.floor(Math.random()*9)
+    for(let i = 1; i<6 ; i++){
         colorStr+= Math.ceil(Math.random()*9)
     }
     return colorStr;
 }
 
-let container = document.querySelector('.boxes');
 function applyColor(){
     for(let i = 0; i < container.children.length; i++){
         container.children[i].style.backgroundColor = generateColor()
         container.children[i].innerText = generateColor()
+        container.children[i].addEventListener('click',(event)=>{
+            body.style.backgroundColor = container.children[i].style.backgroundColor
+        })
+        
     }
 }
 
